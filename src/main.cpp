f2a17d7826874be16af2ac29ae55cfcc6da659ca
PurpleGym01 <kubikmira007@gmail.com>
2025-03-03 17:47:26 +0300

    Int

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int sum(int a, int b);
+int mn(int a, int b);
+
+int main() {
+	int a = 4, b = 10;
+	std::cout << sum(a, b) << std::endl;
+	std::cout << mn(a, b) << std::endl;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,7 @@
+int sum (int a, int b) {
+	return a + b;
+}
+
+int mn (int a, int b) {
+	return a * b;
+}
